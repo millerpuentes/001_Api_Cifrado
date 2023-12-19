fe4e230cd6c15e136e5e2c2b283657b383eca20b
Miller Puentes <miller.puentes@gmail.com>
2023-12-19 08:42:49 -0500

    "desarrollo: test de encriptado-hash!

--- app/main.py
+++ app/main.py
@@ -1,24 +1,49 @@
-"""
-Este es el archivo principal donde crea la API
-"""
+"""Este es el modulo principal donde habrá la creacion y disposición de una API"""
 from fastapi import FastAPI
 from funciones.cifrado_cesar import cifrado_cesar
-app =FastAPI()
+from funciones.encriptado import encriptando_texto
+from funciones.hashing import text_hash
+
+app = FastAPI()
 
 @app.get("/")
 def home():
     """
-    Función prinipal del API
+    Funcion principal de API
     """
-    return{"Hola":"Mundo123"}
+    return {"Hola":"Mundo123"}
 
 @app.get("/cifrado-cesar")
-def url_cifrado_cesar(texto:str,clave:int)->str:
+def url_cifrado_cesar(texto,clave):
     """
     URL para hacer encriptado CESAR
     Se debe ingresar el texto y la clave
     texto (str)
     clave (int)
     """
-    salida = cifrado_cesar(texto, int(clave))
-    return {"mensaje_cifrado ":salida}
+    salida = cifrado_cesar(texto,int(clave))
+    return {"mensaje_cifrado":salida}
+
+@app.get("/encriptado")
+def url_encriptando_texto(mensaje,llave):
+    """En esta url se podrá pedir la encriptación simetrica de un mensaje
+
+    Args:
+        mensaje (str): mensaje a ser encriptado
+        llave (str): llave con la que se hará encriptación simetrica
+
+    Returns:
+        (json): respuesta de la api con mensaje encriptado
+    """
+    salida = encriptando_texto(mensaje.encode('utf-8'),llave.encode('utf-8'))
+    return {"mensaje_encriptado":salida}
+
+@app.get("/text_hash")
+def url_text_hash(mensaje):
+    """Url para hacer hashing de un mensaje con SHA256
+
+    Args:
+        mensaje (str): mensaje a ser hasheado
+    """
+    salida = text_hash(mensaje)
+    return {"mensaje_hash": salida}
--- tests/test_main.py
+++ tests/test_main.py
@@ -9,10 +9,27 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_url_cifrado_cesar():
     """
     Prueba unitaria para verificar api con cifrado cesar
     """
     respuesta = client.get("/cifrado-cesar?texto=hello&clave=3")
     assert respuesta.status_code == 200
-    assert respuesta.json() == {"mensaje_cifrado":"KHÑÑR"}
+    assert respuesta.json() == {"mensaje_cifrado": "KHÑÑR"}
+
+
+def test_url_encriptando_texto():
+    """Prueba unitaria para encriptación de texto"""
+    respuesta = client.get(
+        "/encriptado?mensaje=hola&llave=UyJ4iWnYDPOcZ3sCmwNZtKbIudZRYl5n7owVTiQ4r3o="
+    )
+    assert respuesta.status_code == 200
+
+def test_text_hash():
+    """Esta es la prueba unitaria del modulo de hashing"""
+    respuesta = client.get("/text_hash?mensaje=Hola")
+    esperado = "e633f4fc79badea1dc5db970cf397c8248bac47cc3acf9915ba60b5d76b0e88f"
+    assert respuesta.status_code == 200
+    assert respuesta.json() == {"mensaje_hash": esperado}
+    
